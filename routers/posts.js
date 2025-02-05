9e8ce51ece437ec800cf760508ca60e4f87031dc
Ylenia <yleniamighela@gmail.com>
2025-02-05 15:25:48 +0100

    added operation CRUD

--- routers/posts.js
+++ routers/posts.js
@@ -1,1 +1,43 @@
 // qui andranno tutte le rotte con le operazioni CRUD
+
+const express = require('express')
+const router = express.Router();
+
+
+// index
+router.get('/', function (req, res) {
+    res.send('Lista dei post');
+    });
+
+    // show
+    router.get('/:id', function (req, res) {
+    res.send('Dettagli del post con codice numero ' + req.params.id);
+    });
+
+
+    // store
+    router.post('/', function (req, res) {
+    res.send('Creazione nuovo post');
+
+    });
+
+
+    // update
+    router.put('/:id', function (req, res) {
+    res.send('Modifica del post con codice numero ' + req.params.id);
+    });
+
+
+    // modify
+    router.patch('/:id', function (req, res) {
+    res.send('Modifica parziale del post  con codice numero ' + req.params.id);
+    });
+
+
+    // delete
+    router.delete('/:id', function (req, res) {
+    res.send('Eliminazione del post  con codice numero ' + req.params.id);
+    });
+
+
+    module.exports = router;
