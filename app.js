f8261a6748bb3323681927c6c72b439e6df3dfbb
Ylenia <yleniamighela@gmail.com>
2025-02-05 14:59:53 +0100

    server settings

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
 
+// definisco la cartella per i file statici
+app.use(express.static('public'));
+
+
+// definsco la prima rotta con un semplice messaggio
+app.get('/', (req, res) => {
+
+    res.send("Server del mio blog")
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+    });
